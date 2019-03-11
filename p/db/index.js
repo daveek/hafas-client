8d11d64a27d6618032edef52148254a36ae8d71d
Jannis R <mail@jannisr.de>
2019-03-11 11:10:21 +0100

    DB journeys: make default price null :boom:
    closes #91

--- p/db/index.js
+++ p/db/index.js
@@ -62,6 +62,7 @@ const createParseJourney = (profile, opt, data) => {
 	const parseJourneyWithPrice = (j) => {
 		const res = parseJourney(j)
 
+		res.price = null
 		// todo: find cheapest, find discounts
 		// todo: write a parser like vbb-parse-ticket
 		// [ {
@@ -72,7 +73,6 @@ const createParseJourney = (profile, opt, data) => {
 		// 	targetCtx: 'D',
 		// 	buttonText: 'To offer selection'
 		// } ]
-		res.price = {amount: null, hint: 'No pricing information available.'}
 		if (
 			j.trfRes &&
 			Array.isArray(j.trfRes.fareSetL) &&
